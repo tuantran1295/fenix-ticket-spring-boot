f565e657acb1670928e65a39026ce433d6de76a5
Trần Tuấn <tranquoctuan@MacBook-Air-cua-Tran.local>
2025-06-06 13:49:41 +0700

    add seed data to flyway

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -3,18 +3,18 @@
 -- GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES ON DATABASE fenixdb TO fenix;
 
 -- Schema
-CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL
-    );
-
-CREATE TABLE IF NOT EXISTS tickets (
-                                       id SERIAL PRIMARY KEY,
-                                       title VARCHAR(250) NOT NULL,
-    description VARCHAR(250) NOT NULL,
-    status VARCHAR(250) NOT NULL
-    );
+-- CREATE TABLE IF NOT EXISTS users (
+--                                      id SERIAL PRIMARY KEY,
+--                                      username VARCHAR(50) NOT NULL UNIQUE,
+--     password VARCHAR(100) NOT NULL
+--     );
+--
+-- CREATE TABLE IF NOT EXISTS tickets (
+--                                        id SERIAL PRIMARY KEY,
+--                                        title VARCHAR(250) NOT NULL,
+--     description VARCHAR(250) NOT NULL,
+--     status VARCHAR(250) NOT NULL
+--     );
 
 -- Privileges: give fenix full rights on all future tables,
 -- and on all existing tables, explicit for migration idempotence.
@@ -31,3 +31,13 @@ CREATE TABLE IF NOT EXISTS tickets (
 INSERT INTO users (username, password)
 VALUES ('admin', 'password')
     ON CONFLICT (username) DO NOTHING;
+
+INSERT INTO tickets (description, status, title)
+VALUES ('Example ticket description 1', 'OPEN', 'Issue Example 1'),
+       ('Example ticket description 2', 'OPEN', 'Issue Example 2'),
+       ('Example ticket description 3', 'CLOSE', 'Issue Example 3'),
+       ('Example ticket description 4', 'OPEN', 'Issue Example 4'),
+       ('Example ticket description 5', 'OPEN', 'Issue Example 5')
+
+    ON CONFLICT (username) DO NOTHING;
+
